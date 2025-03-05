d8b2bea6ca23b13b701abd8b761c4c2d156bd4f3
laowu <wuly93@163.com>
2025-03-05 11:48:17 +0800

    feat: add 子应用

---
+++ applications/app-demo-3/config/dev.config.ts
@@ -0,0 +1,29 @@
+
+// 使用mock.js api 模拟(只在dev环境生效)
+export const USE_MOCK_API = true;
+// 配置本地dev代理转发服务器
+export const DEV_PORXY_CONFIG = {};
+
+// export const DEV_PORXY_CONFIG = {
+//   '/api': {
+//     // 多路径代理配置
+//     target: 'http://api.example.com',
+//     changeOrigin: true,
+//     rewrite: (path) => path.replace(/^\/api/, ''),
+//   },
+
+//   '/uploads': {
+//     target: 'http://static.example.com',
+//     changeOrigin: true,
+//   },
+//   '^/user/.*': {
+//     // 通过正则表达式灵活匹配复杂路径：
+//     // 匹配所有以 /user/ 开头的请求
+//     target: 'http://user-service.com',
+//     changeOrigin: true,
+//   },
+//   '/socket': {
+//     target: 'ws://your-websocket-server',
+//     ws: true, //配置 ws: true 可代理 WebSocket 请求：
+//   },
+// };
